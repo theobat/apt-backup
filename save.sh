7a855f354a9cd3d9d3f03f5a08538ed1193b5cb2
theobat <theophile.batoz@gmail.com>
2016-09-27 18:35:30 +0200

    providing saving name -n arg

--- save.sh
+++ save.sh
@@ -1,4 +1,26 @@
-dpkg --get-selections > ./${1}/Package.list
-sudo cp -R /etc/apt/sources.list* ./${1}
-sudo apt-key exportall > ./${1}/Repo.keys
+SYSTEM_NAME="ubuntu16"
+while getopts ":n:" opt; do
+  case $opt in
+    n)
+      SYSTEM_NAME=$OPTARG
+      echo "Using specifyed name, Parameter: $OPTARG" >&2
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
 
+
+if [ ! -d "$SYSTEM_NAME" ]; then
+    mkdir ${SYSTEM_NAME}
+fi
+echo "Name being used is $SYSTEM_NAME" >&2
+dpkg --get-selections > ./${SYSTEM_NAME}/Package.list
+sudo cp -R /etc/apt/sources.list* ./${SYSTEM_NAME}
+sudo apt-key exportall > ./${SYSTEM_NAME}/Repo.keys
